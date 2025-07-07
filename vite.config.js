a2a28a9be725e4c1d934a183243f1cfbd91c6695
Theo <theotorku1983@gmail.com>
2025-07-07 12:42:14 -0500

    theos-portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // use this if you plan to deploy to GitHub Pages
+  base: '/' // use this if you plan to deploy to GitHub Pages
 })
